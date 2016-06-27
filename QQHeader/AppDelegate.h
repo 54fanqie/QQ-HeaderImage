dd4c633edca24b9ec4b8ddd992094b77c60ef501
54fanqie <2917545349@qq.com>
2016-06-27 16:12:11 +0800

    Initial Commit

---
+++ QQHeader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQHeader
+//
+//  Created by macmini2 on 16/6/27.
+//  Copyright © 2016年 emiage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
